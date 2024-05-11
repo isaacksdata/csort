ea4fa8aef68347bac239906246a24380c30a77a0
isaacpersonal <isaacks123@gmail.com>
2024-05-11 10:42:36 +0100

    tests: adding tests for main.py

---
+++ tests/test_main.py
@@ -0,0 +1,110 @@
+import argparse
+import logging
+import os
+import shutil
+import sys
+from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+
+from src.main import main
+from src.main import parse_commandline
+from src.main import validate_paths
+
+
+DEBUG = os.getcwd().endswith("tests")
+
+
+@pytest.fixture
+def script_path():
+    if DEBUG:
+        return "scripts/basic_input.py"
+    return "tests/scripts/basic_input.py"
+
+
+@pytest.fixture
+def output_path():
+    if DEBUG:
+        return "scripts/output/basic_output.py"
+    return "tests/scripts/output/basic_output.py"
+
+
+def test_parse_commandline():
+    commands = [
+        "",  # mock script name
+        "--input-path=test/input/path/file.py",
+        "--output-path=test/output/path/file.py",
+        "--config-path=test/config/csort.ini",
+        "--skip-patterns=expected",
+        "--skip-patterns=pattern",
+    ]
+    with patch.object(sys, "argv", commands):
+        output = parse_commandline()
+    assert isinstance(output, argparse.Namespace)
+    assert output.input_path == "test/input/path/file.py"
+    assert output.output_path == "test/output/path/file.py"
+    assert output.config_path == "test/config/csort.ini"
+    assert output.skip_patterns == ["expected", "pattern"]
+
+
+def test_validate_paths(script_path, output_path):
+    inputs, outputs = validate_paths(script_path, output_path)
+    assert isinstance(inputs, list)
+    assert isinstance(outputs, list)
+    assert len(inputs) == len(outputs) == 1
+    assert inputs[0] == script_path
+    assert outputs[0] == output_path
+
+
+def test_validate_paths_input_dir_output_file(script_path, output_path):
+    script_path = Path(script_path).parent.as_posix()
+    with pytest.raises(ValueError):
+        validate_paths(script_path, output_path)
+
+
+def test_validate_paths_input_dir_output_dir(script_path, output_path):
+    script_path = Path(script_path).parent.as_posix()
+    output_path = Path(output_path).parent.as_posix()
+    inputs, outputs = validate_paths(script_path, output_path)
+    assert isinstance(inputs, list)
+    assert isinstance(outputs, list)
+    assert len(inputs) == len(outputs)
+    assert all(Path(i).stem == Path(o).stem for i, o in zip(inputs, outputs))
+
+
+def test_validate_paths_input_file_output_dir(script_path, output_path):
+    output_path = Path(output_path).parent.as_posix()
+    inputs, outputs = validate_paths(script_path, output_path)
+    assert isinstance(inputs, list)
+    assert isinstance(outputs, list)
+    assert len(inputs) == len(outputs)
+    assert (
+        outputs[0]
+        == Path(script_path).parent.joinpath(Path(output_path).stem).joinpath(Path(script_path).name).as_posix()
+    )
+
+
+def test_validate_paths_not_exist():
+    input_path = "input/script.py"
+    with pytest.raises(FileNotFoundError):
+        validate_paths(input_path)
+
+
+def test_main_no_scripts(caplog):
+    os.mkdir("empty_dir")
+    commands = ["", "--input-path=./empty_dir"]  # mock script name
+    with patch.object(sys, "argv", commands):
+        main()
+    assert "No Python scripts found in ./empty_dir" in caplog.messages
+    shutil.rmtree("./empty_dir")
+
+
+def test_main(script_path, output_path, caplog):
+    caplog.set_level(logging.DEBUG)
+    commands = ["", f"--input-path={script_path}", f"--output-path={output_path}"]  # mock script name
+    with patch.object(sys, "argv", commands):
+        main()
+    assert Path(output_path).exists()
+    assert f"Reformatting {script_path} ..." in caplog.messages
+    shutil.rmtree(Path(output_path).parent.as_posix())
