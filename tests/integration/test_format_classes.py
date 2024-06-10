4103e19eea4436358105e983b1cd527a7485da8c
isaacpersonal <isaacks123@gmail.com>
2024-06-10 08:14:50 +0100

    tests: adding tests for class definitions nested in functions

--- tests/integration/test_format_classes.py
+++ tests/integration/test_format_classes.py
@@ -375,3 +375,27 @@ def test_formatting_csort_nested_classes_ast(parser, method_describer, input_pat
 def test_formatting_csort_nested_classes_cst(parser, method_describer, input_path, output_path, expected_path, caplog):
     caplog.set_level(logging.INFO)
     simple_test(parser, method_describer, input_path, output_path, expected_path, use_cst=True, auto_static=False)
+
+
+@pytest.mark.parametrize("parser", ["ast"], indirect=True)
+@pytest.mark.parametrize("method_describer", ["ast"], indirect=True)
+@pytest.mark.parametrize("input_path", ["nested_function_classes"], indirect=True)
+@pytest.mark.parametrize("output_path", ["nested_function_classes"], indirect=True)
+@pytest.mark.parametrize("expected_path", ["nested_function_classes"], indirect=True)
+def test_formatting_csort_nested_function_classes_ast(
+    parser, method_describer, input_path, output_path, expected_path, caplog
+):
+    caplog.set_level(logging.INFO)
+    simple_test(parser, method_describer, input_path, output_path, expected_path, use_cst=False, auto_static=False)
+
+
+@pytest.mark.parametrize("parser", ["cst"], indirect=True)
+@pytest.mark.parametrize("method_describer", ["cst"], indirect=True)
+@pytest.mark.parametrize("input_path", ["nested_function_classes"], indirect=True)
+@pytest.mark.parametrize("output_path", ["nested_function_classes"], indirect=True)
+@pytest.mark.parametrize("expected_path", ["nested_function_classes"], indirect=True)
+def test_formatting_csort_nested_function_classes_cst(
+    parser, method_describer, input_path, output_path, expected_path, caplog
+):
+    caplog.set_level(logging.INFO)
+    simple_test(parser, method_describer, input_path, output_path, expected_path, use_cst=True, auto_static=False)
